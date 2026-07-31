69942e7492772c1b912c285a3e1344eafc1bf1a5
vishal33t <rajputvishal33t@gmail.com>
2026-08-01 00:05:49 +0530

    correct second largest number dsa problem output

--- module3coredsa/08secondlargest.py
+++ module3coredsa/08secondlargest.py
@@ -1,7 +1,15 @@
+# find second largest number
+
 search_arr =[int(x) for x in input("enter number by follow space").split()]
-secLargest =search_arr[0]
+if len(search_arr) <2:
+    raise ValueError("list must be greater than 2")
+first = second = float('-inf')      
 for num in search_arr:
-    if secLargest<num:
-        secLargest=num
-print(secLargest)        
-         
+    if num>first:
+        second = first
+        first=num
+    elif num > second and num !=first:
+        second = num   
+    if second == float('-inf'):
+        raise ValueError("No second largest element found (all elements may be equal)") 
+print(second)
