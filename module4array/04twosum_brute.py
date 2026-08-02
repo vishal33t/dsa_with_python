93f0740110539d16a938bf8c10fc7aee1588dd1b
vishal33t <rajputvishal33t@gmail.com>
2026-08-02 22:28:59 +0530

    solving two sum problem using brute force method

---
+++ module4array/04twosum_brute.py
@@ -0,0 +1,13 @@
+#Problem 4: Two Sum ⭐⭐⭐⭐⭐
+#This is probably the most famous interview problem.
+#Given arr = [2,7,11,15]    target = 9
+#Output [0,1]
+def twosum(n):
+    arr = [2,7,11,15]
+    target = 9
+    for i in range (len(arr)-1):
+     for j in range (i+1,len(arr)-1):
+        if arr[i] + arr[j] == target:
+            print(i,j)
+arr = [2,7,11,15]   
+twosum(arr)
